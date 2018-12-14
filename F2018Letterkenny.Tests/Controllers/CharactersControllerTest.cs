9e84cb52351a0ea53948faf2580f7124719ced47
Harleen Singh <harleensingh315@gmail.com>
2018-12-14 10:39:49 -0500

    finaltest commit

---
+++ F2018Letterkenny.Tests/Controllers/CharactersControllerTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using F2018Letterkenny.Controllers;
+using F2018Letterkenny.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace F2018Letterkenny.Tests.Controllers
+{
+    [TestClass]
+    public class CharactersControllerTest
+    {
+        CharactersController controller;
+        Mock<IMockCharacter> mock;
+        List<Character> characters;        
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mock = new Mock<IMockCharacter>();
+
+            characters = new List<Character>
+            {
+                new Character
+                {
+                    CharacterId = 43,
+                    Name = "Reilly",
+                    Description = "Hockey Player with Flow",
+                    Quote = "Wheel, snipe, celly boys",
+                    Photo = "reilly.png"
+                },
+                new Character
+                {
+                    CharacterId = 43,
+                    Name = "Jonesy",
+                    Description = "Even Dumber Hockey Player",
+                    Quote = "Backcheck, Forecheck, Paycheque",
+                    Photo = "jonesey.png"
+                }
+            };
+
+            mock.Setup(m => m.Characters).Returns(characters.AsQueryable());
+            controller = new CharactersController(mock.Object);
+        }
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+        [TestMethod]
+        public void IndexValidLoadsCharacter()
+        {
+            var actual = (List<Character>)((ViewResult)controller.Index()).Model;
+            CollectionAssert.AreEqual(characters, actual);
+        }
+
+        public void Create_ViewLoads()
+        {
+            var result = (ViewResult)controller.Create();
+            Assert.AreEqual("Create", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Create_Validcharacter()
+        {
+            Character newCharacter = new Character
+            {
+                CharacterId = 4,
+                Name = "Four",
+                City = "London",
+                Url = "http://three.ca",
+                Logo = "three.gif"
+            };
+            RedirectToRouteResult result = (RedirectToRouteResult)controller.Create(newCharacter);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Create_InValidCharacter()
+        {
+            Character newCharacter = new Character();
+            controller.ModelState.AddModelError("Unable To Create Grade", "Grade Creation Exception");
+            ViewResult result = (ViewResult)controller.Create(newCharacter);
+            Assert.AreEqual("Create", result.ViewName);
+        }
+
+        
+    }
+}
+    
+
---
+++ F2018Letterkenny/Models/EFCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace F2018Letterkenny.Models
+{
+    public class EFCharacter : IMockCharacter
+    {
+        private DatabaseModel db = new DatabaseModel();
+        IQueryable<Character> IMockCharacter.Characters { get { return db.Characters;  } }
+
+        Character IMockCharacter.Save(Character character)
+        {
+            if (character.CharacterId == 0)
+            {
+                db.Characters.Add(character);
+            }
+            else
+            {
+                db.Entry(character).State = System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
+
+
+
+
+
+
+
+
+
+
+        }
+        
+        
+    }
+}
+
+
+
+         
